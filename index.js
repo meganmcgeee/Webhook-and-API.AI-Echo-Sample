5b3100ddf6fb034aea77cff577189cbf5817bd80
meganmcgeee <meganpmcgee@gmail.com>
2018-03-05 13:15:47 -0800

    Renamed 'userInput' variables to 'speech because webhook not fulfilling.

--- index.js
+++ index.js
@@ -14,15 +14,14 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  let userInput =
+  let speech =
     req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "I don't think I can help you with that yet. Type 'HELP' again for more on how I can help.";
   return res.json({
-    userInput: userInput,
-    displayText: userInput,
+    speech: speech,
+    displayText: speech,
     source: "webhook-echo-sample"
   });
 });
-
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
